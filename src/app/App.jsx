88fb34e330848ac16683d0f04afb9580e258cb2a
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-05-02 04:11:29 -0300

    Adjusting styles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,22 +4,22 @@ const users = JSON.stringify(Users());
 
 function App() {
   return (
-    <div className="dark bg-gray-50 font-sans text-stone-900 dark:bg-stone-900 dark:text-gray-50">
-      <div>
-        <h1 className="pt-10 pb-6 text-4xl">Large Grid Vite React</h1>
-      </div>
-      <div className="container">
+    <div className="dark bg-gray-50 font-sans text-stone-900 dark:bg-stone-900 dark:text-gray-50 w-full  h-screen">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="pt-10 pb-6 text-5xl font-bold mx-auto text-center">Large Grid Vite React</h1>
         <div className="header">&nbsp;</div>
         <div className="main">{users}</div>
-        {/* {Timing(2) ? (
-            <div className="preLoader">
-              <Spinner />
-            </div>
-          ) : (
-            <div className="main">
-              <Users />
-            </div>
-          )} */}
+        <div className="container">
+          {/* {Timing(2) ? (
+              <div className="preLoader">
+                <Spinner />
+              </div>
+            ) : (
+              <div className="main">
+                <Users />
+              </div>
+            )} */}
+        </div>
       </div>
     </div>
   );
