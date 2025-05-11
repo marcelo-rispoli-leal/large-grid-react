78ff977b5bf2978c1553264c0682b703c1217672
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-05-11 05:42:45 -0300

    Adjusted number format in Summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,10 +3,10 @@ export default function Summary({ count, lines }) {
     <div className="b-std bg-cyan-700 p-3">
       <div className="grid h-full items-center text-neutral-100">
         <div>
-          Users Founded: <strong>{count}</strong>
+          Users Founded: <strong>{count.toLocaleString("pt-BR")}</strong>
         </div>
         <div>
-          Number of Lines: <strong>{lines}</strong>
+          Number of Lines: <strong>{lines.toLocaleString("pt-BR")}</strong>
         </div>
       </div>
     </div>
