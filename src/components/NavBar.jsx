95bba08bc69517b5372fdc83f6dbddd45f547c2f
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-05-14 12:11:58 -0300

    Ignored prettier on NavBar function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,13 @@ import ToggleTheme from "./ToggleTheme";
 //prettier-ignore
 const { VITE_EMAIL_LINK, VITE_GITHUB_LINK, VITE_LINKEDIN_LINK } = import.meta.env;
 
+//prettier-ignore
 export default function NavBar() {
   return (
     <div className="absolute top-0 right-0 flex gap-2">
-      {VITE_EMAIL_LINK && (
-        <IconLink icon={<BsEnvelope />} link={`mailto:${VITE_EMAIL_LINK}`} />
-      )}
-      {VITE_GITHUB_LINK && (
-        <IconLink icon={<BsGithub />} link={VITE_GITHUB_LINK} />
-      )}
-      {VITE_LINKEDIN_LINK && (
-        <IconLink icon={<BsLinkedin />} link={VITE_LINKEDIN_LINK} />
-      )}
+      {VITE_EMAIL_LINK && (<IconLink icon={<BsEnvelope />} link={`mailto:${VITE_EMAIL_LINK}`} />)}
+      {VITE_GITHUB_LINK && (<IconLink icon={<BsGithub />} link={VITE_GITHUB_LINK} />)}
+      {VITE_LINKEDIN_LINK && (<IconLink icon={<BsLinkedin />} link={VITE_LINKEDIN_LINK} />)}
       <ToggleTheme />
     </div>
   );
