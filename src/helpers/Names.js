25575ef8e8cef5aebb90ee41facfee3886547917
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-05-18 14:46:01 -0300

    Updated Names.js to use new enviroment variables

--- src/helpers/Names.js
+++ src/helpers/Names.js
@@ -1,15 +1,14 @@
 //This function returns the random name for all users
 import { faker } from "@faker-js/faker/locale/pt_BR";
 
-const getNames = () => {
-  const limit = ~~Number(import.meta.env.VITE_USERS_LIMIT);
-  const count = limit > 0 ? limit : 1000;
+export default function Names() {
+  const USER_LIMIT = Number(import.meta.env.VITE_USERS_LIMIT) || 10000;
+  const BATCH_SIZE = Math.min(
+    Number(import.meta.env.VITE_USERS_FREQUENCY) || 1000,
+    USER_LIMIT,
+  );
   return faker.helpers.multiple(
     () => faker.person.firstName() + " " + faker.person.lastName(),
-    { count: count },
+    { count: BATCH_SIZE },
   );
-};
-
-export default function Names() {
-  return getNames();
 }
